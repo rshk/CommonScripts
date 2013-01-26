23399ce1660b76f1744375eee8d92559190a85ef
Samuele Santi <redshadow@hackzine.org>
2013-01-26 01:00:00 +0100

    never assume things will work.. :(

--- Linux/Generic/genkernel-install-kernel.sh
+++ Linux/Generic/genkernel-install-kernel.sh
@@ -1,25 +1,51 @@
-#!/bin/bash
+#!/bin/sh
 
+##==============================================================================
 ## Simple genkernel command to fully install kernel from the current
 ## directory. This is useful when manually configuring and building
 ## kernel, but still wanting to use some genkernel features.
 ##
 ## Just run this from /usr/src/linux -- you'll find everything
 ## installed in your /boot/
+##
+## This is the better version, that should do pretty much everything..
+## Unluckily, we cannot interleave comments in commands.. :(
+##==============================================================================
 
-#genkernel --no-clean --no-mrproper --kernel-config=.config --install all
+#genkernel \
+#    # If we already built this kernel
+#    --no-clean \
+#    # If we already configured this kernel
+#    --no-mrproper \
+#    # If we didn't configure the kernel yet
+#    --oldconfig \
+#    # This is the default..
+#    --kernel-config=.config \
+#    # We want to configure grub
+#    --bootloader=grub \
+#    # We want the splash screen
+#    --splash \
+#    # Install after building
+#    --install \
+#    # Be verbose!
+#    --loglevel=5 \
+#    # Where is the kernel dir?
+#    --kerneldir=. \
+#    # Standard MAKEOPTS
+#    --makeopts='-j5' \
+#    # Yeah, build everything!
+#    all
 
 
-## This is the better version, that should do pretty much everythin
 genkernel \
-    --no-clean \ # If we already built this kernel
-    --no-mrproper \ # If we already configured this kernel
-    --oldconfig \ # If we didn't configure the kernel yet
-    --kernel-config=.config \ # This is the default..
-    --bootloader=grub \ # We want to configure grub
-    --splash \ # We want the splash screen
-    --install \ # Install after building
-    --loglevel=5 \ # Be verbose!
-    --kerneldir=. \ # Where is the kernel dir?
-    --makeopts='-j5' \ # Standard MAKEOPTS
-    all # Yeah, build everything!
+    --no-clean \
+    --no-mrproper \
+    --oldconfig \
+    --kernel-config=.config \
+    --bootloader=grub \
+    --splash \
+    --install \
+    --loglevel=5 \
+    --kerneldir=. \
+    --makeopts='-j5' \
+    all
