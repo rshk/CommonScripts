9b51fad79977c435ed7dbf4a652196e0b9d92175
Samuele Santi <redshadow@hackzine.org>
2013-03-18 22:16:09 +0100

    Added sources.list for GlusterFS
    Now supporting per-release repositories too.

--- Linux/Debian/install-apt-conf.sh
+++ Linux/Debian/install-apt-conf.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 BASEDIR="$( dirname "$0" )"
+RELEASE="$( lsb_release -sc )"
 
 echo "Copying sources.list"
-cp "$BASEDIR"/apt-sources-list-d/*.list /etc/apt/sources.list.d/
+cp "$BASEDIR"/apt-sources-list-d/all/*.list /etc/apt/sources.list.d/
+if [ -d "$BASEDIR"/apt-sources-list-d/"$RELEASE"/ ]; then
+	cp "$BASEDIR"/apt-sources-list-d/"$RELEASE"/*.list /etc/apt/sources.list.d/
+else
+	echo "WARNING! No release-specific folder found for $RELEASE"
+fi
 
 echo "Copying preferences"
 cp "$BASEDIR"/apt-preferences-d/*.pref /etc/apt/preferences.d/
