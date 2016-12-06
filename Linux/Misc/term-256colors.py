7bd72eeae6ef8710c32269b833d79caaf9b5f11e
Samuele Santi <redshadow@hackzine.org>
2016-12-06 14:31:40 +0000

    Improve script to print 256col table

--- Linux/Misc/term-256colors.py
+++ Linux/Misc/term-256colors.py
@@ -14,23 +14,26 @@ def format_cell(c):
             .format(color, 255 if is_dark(r, g, b) else 232, str(color)))
 
 
+def luminance(r, g, b):
+    return (0.299 * r / 5) + (0.587 * g / 5) + (0.114 * b / 5)
+
+
 def is_dark(r, g, b):
-    return 0.2126 * r + 0.7152 * g + 0.0722 * b < 2
+    return luminance(r, g, b) < .4
 
 
-for c1 in range(6):
-    for c2 in range(6):
-        for perm in range(3):
-            for c3 in range(6):
-                if perm == 0:
-                    color = (c1, c2, c3)
-                elif perm == 1:
-                    color = (c2, c3, c1)
-                elif perm == 2:
-                    color = (c3, c1, c2)
-
-                print(format_cell(color), end='')
-
-            print('  ', end='')
+if __name__ == '__main__':
+    for c1 in range(6):
+        for c2 in range(6):
+            for perm in range(3):
+                for c3 in range(6):
+                    if perm == 0:
+                        color = (c1, c2, c3)
+                    elif perm == 1:
+                        color = (c2, c3, c1)
+                    elif perm == 2:
+                        color = (c3, c1, c2)
+                    print(format_cell(color), end='')
+                print('  ', end='')
+            print()
         print()
-    print()
